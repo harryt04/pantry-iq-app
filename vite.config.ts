b56758904272c3cf2cd698ed0423f0158b3dbbad
Harry Thomas <hsthomas@verisk.com>
2026-02-17 10:00:38 -0700

    fix vite config for nitro

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,18 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import { fileURLToPath, URL } from 'node:url'
+import { nitro } from 'nitro/vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
 import tailwindcss from '@tailwindcss/vite'
+import { devtools } from '@tanstack/devtools-vite'
 
-const config = defineConfig({
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+export default defineConfig(({ mode }) => ({
   plugins: [
     devtools(),
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
     tailwindcss(),
-    tanstackStart(),
+    tsConfigPaths({ projects: ['./tsconfig.json'] }),
+    tanstackStart({ srcDirectory: 'src' }),
+    mode === 'production' ? nitro() : null, // Using the nitro plugin breaks vitest, so only enable it in production
     viteReact(),
   ],
-})
-
-export default config
+}))
